c578b91f0af623a8364636cb201e395a0d55930e
Gabriel Gordon <gabegor94@gmail.com>
2019-12-03 16:09:57 +0200

    adding responsiveness

--- js/mincraft.js
+++ js/mincraft.js
@@ -209,7 +209,6 @@ Minecraft.getLeftBox = function (currentBox) {
             return boxsList[i];
         }
     }
-    console.log('here');
 }
 Minecraft.getRightBox = function (currentBox) {
     let boxsList = document.getElementsByClassName('box');
